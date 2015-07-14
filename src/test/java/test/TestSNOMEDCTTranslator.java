cdfb5f5b00eb9c905b8e203f859af696f6b5b32e
danka74 <daniel.e.karlsson@gmail.com>
2015-07-14 08:27:22 +0200

    Added assertion in roundtrip test

--- src/test/java/test/TestSNOMEDCTTranslator.java
+++ src/test/java/test/TestSNOMEDCTTranslator.java
@@ -3,6 +3,12 @@
  */
 package test;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.semanticweb.owlapi.model.OWLException;
 
@@ -17,12 +23,15 @@ public class TestSNOMEDCTTranslator {
 	/**
 	 * Test method for {@link se.liu.imt.mi.snomedct.expression.tools.SNOMEDCTTranslator#main(java.lang.String[])}.
 	 * @throws OWLException 
+	 * @throws IOException 
 	 */
 	@Test
-	public void testMain() throws OWLException {
+	public void testMain() throws OWLException, IOException {
 		SNOMEDCTTranslator.main(new String[] {"-l", "-f", "sct", "-n", "stated", "src/test/resources/hernia_repair_module.owl"});
 		SNOMEDCTTranslator.main(new String[] {"-f", "owlf", "src/test/resources/hernia_repair_module_sct_stated.owl"});
-
+		File file1 = new File("src/test/resources/hernia_repair_module.owl");
+		File file2 = new File("src/test/resources/hernia_repair_module_sct_stated_owlf_stated.owl");
+		assertTrue("Files differ!", FileUtils.contentEquals(file1, file2));
 	}
 
 }
