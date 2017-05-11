25a1aa2c67cf5a4ee54a7a7acad45a109c6f0b19
danka74 <daniel.karlsson@liu.se>
2017-05-11 15:38:02 +0200

    updated list of never grouped attributes

--- src/main/java/se/liu/imt/mi/snomedct/parser/OWLVisitor.java
+++ src/main/java/se/liu/imt/mi/snomedct/parser/OWLVisitor.java
@@ -60,11 +60,7 @@ public class OWLVisitor extends SNOMEDCTExpressionBaseVisitor<OWLObject> {
 
 	private static final String[] neverGrouped = { SCTID_IRI + "123005000",
 			SCTID_IRI + "272741003", SCTID_IRI + "127489000",
-			SCTID_IRI + "704347000", SCTID_IRI + "704318007",
-			SCTID_IRI + "704319004", SCTID_IRI + "123454321",
-			SCTID_IRI + "704327008", SCTID_IRI + "370132008",
-			SCTID_IRI + "246501002", SCTID_IRI + "411116001",
-			SCTID_IRI + "704346009", SCTID_IRI + "704321009" };
+		    SCTID_IRI + "411116001" };
 
 	static Logger logger = Logger.getLogger(OWLVisitor.class);
 
